9aab37385815fe921b587b394616f5d76b06d3f7
Shohjahon <shohjahonrazzoqov@gmail.com>
2022-09-13 15:08:52 -0400

    first commit

---
+++ dart_examples_ifelse.dart
@@ -0,0 +1,49 @@
+void main() {
+  
+// int number = 100;
+// number = 100.0;
+
+//type berilgan o'zgaruvchiga boshqa typedagi o'zgaruvchini o'zlashtirib bo'lmaydi
+
+// double number1 = 10.0;
+// number1 = 10;
+
+// 1 If ELSE IF ElSE
+//bir qiymatni ikkinchi qiymat bilan solishtirish
+
+  int number = 100;
+  int number2 = 200;
+
+  if (number > number2) {
+    print(number);
+  } else if (number < number2) {
+    print(number2);
+  } else {
+    print("Hello dart");
+  }
+
+// 2 TERNERY IF
+
+  number > number2 ? number : number2;
+  //va buni bir qiymatga tenglab yuborish mumkin.
+
+  int kattaSon = number > number2 ? number : number2;
+  
+
+//3 SWITCH CASE
+
+  int son = 1;
+  switch (son) {
+    case 0:
+      print("Son = $son");
+      break;
+    case 1:
+      print("Son = $son");
+      break;
+    case 2:
+      print("Son = $son");
+      break;
+    default:
+      print("Qiymat yo'q");
+  }
+}
---
+++ dart_variables.dart
@@ -0,0 +1,50 @@
+void main() {
+
+  
+  //O'zgaruvchi = Qiymatlarni RAMda saqlaydi
+
+  //DECLARE     = O'zgaruvchini tanitish
+  int numbers;
+
+  //ASSIGNMENT  = O'zgaruvchiga qiymat berish
+  numbers = 23;
+
+
+
+  //FINAL   - Ishlatmaguncha RAM dan joy ajratmaydi
+
+  //CONST   - Elon qilinishi bilan RAMdan joy egallaydi 
+
+  //NUMBER  - Int = 10, Double = 10.0,
+
+  //STRING  - "ABS", '''ABS''', 'ABS',
+
+  //BOOLEAN - TRUE FALSE,
+
+  //LIST    - [12,"10",6.5],
+
+  //MAP     - "Name" : "Abdulloh",
+
+  //RUNES   - String Typeidagi UniCode Caracterlarni saqlaydi,
+
+  //ONJECT  - Dartdagi barcha o'zgaruvchilar OBJECT classidan ildiz olingan holatda chiqariladi
+
+  //VAR     - O'zgaruvchini typeni berishni istamasa
+
+  //VAR     - VARning Typeni compiler aniqlaydi
+
+  //HEXDECIMA 16 sanoq sestimasidagi son  0xAABBSS;
+
+  //PRIMITIVE - Int, Double, String, BOOLEAN,
+
+  //RECERENCE - List, Map, Set,
+
+  //RECERENCE =  List l = [11, 11, 11];
+  //              List l2 = l;
+  //Agar l2 o'zgartirsam l ning qiymati ham o'zgaradi
+
+  //String Interpolation - Sting ichida amallar bajarish
+
+  print("Hi $numbers");
+  print("Hi ${12 + 10}");
+}
